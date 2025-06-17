10aeb3a3f400eb26c7d1572b55e1c819445aefb7
Sergey Peresypkin <93827706+Segaoneboy@users.noreply.github.com>
2025-06-17 18:16:29 +0500

    17.06.25

--- controllers/GetAllCardsFunction.js
+++ controllers/GetAllCardsFunction.js
@@ -3,16 +3,36 @@ const GetAllCards = (req, res) =>{
     let counter = 0;
     const { PrismaClient } = require('@prisma/client');
     const prisma = new PrismaClient();
+    const limit = 10
+    const page = parseInt(req.query.page) || 1;
+    const skip = (page-1) * limit;
     prisma.card.findMany({
+        skip: skip,
+        take: limit,
+        orderBy: {
+            createdAt: 'desc',
+        }
     })
         .then((cards) => {
-            cards.map(card => {
-
-            })
+            prisma.card.count()
+                .then(total => {
+                    res.status(200).json({
+                        cards,
+                        total,
+                        page,
+                        totalPages: Math.ceil(Math.ceil(total / limit)),
+                    })
+                })
+                .catch(err => {
+                    res.status(500).json({ error: 'Ошибка сервера при подсчёте'})
+                })
         })
         .catch((err) =>{
             console.error("Ошибка при получении карточек", err);
             res.status(500).json({error: "Ошибка сервера"})
+        })
+        .finally(() => {
+            prisma.$disconnect();
         })
 }
 module.exports = GetAllCards
