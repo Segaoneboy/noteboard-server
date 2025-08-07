b6ffa369d84df2aa858f6eed306dab640cb8474d
Sergey Peressypkin <93827706+Segaoneboy@users.noreply.github.com>
2025-08-07 11:50:23 +0500

    cookies store fix5

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -60,13 +60,13 @@ const login = (req, res) =>{
                     res.cookie("refreshToken", refreshToken, {
                         httpOnly: true,
                         secure: true,
-                        sameSite: 'strict',
+                        sameSite: "none",
                         maxAge: 7*24*60*60*1000,
                     })
                     res.cookie('accessToken', accessToken, {
                         httpOnly: true,
                         secure: true,
-                        sameSite: 'strict',
+                        sameSite: "none",
                         maxAge: 10*60*1000,
                     })
                     res.json({message: "Успешный вход"})
